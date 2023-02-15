a042ac56f449b81993e085b8e96c5264b40daf32
littleblus <gqstudio004@foxmail.com>
2023-02-15 16:16:30 +0800

    P1075 质因数分解

---
+++ 洛谷官方题单/入门3循环结构/P1720/main.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <cstdio>
+#include <cmath>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%.2f\n", ((pow((1 + sqrt(5)) / 2, n)) - (pow((1 - sqrt(5)) / 2, n))) / sqrt(5));
+	return 0;
+}
