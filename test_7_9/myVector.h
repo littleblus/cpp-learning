8b562f425290f915128a4e361c66886d6b8ae301
littleblus <gqstudio004@foxmail.com>
2023-07-13 18:42:18 +0800

    vector的模拟实现

--- test_7_9/myVector.h
+++ test_7_9/myVector.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <assert.h>
+#include <iostream>
+
 namespace blus {
 	template<typename T>
 	class vector {
@@ -13,25 +16,121 @@ namespace blus {
 			, _endOfStorage(nullptr)
 		{}
 
+		// 拷贝构造
+		vector(const vector<T>& other)
+			: _start(nullptr)
+			, _finish(nullptr)
+			, _endOfStorage(nullptr)
+		{
+			*this = other;
+ 		}
+
+		// 赋值重载
+		vector<T>& operator=(const vector<T>& other) {
+			if (this != &other) {
+				size_t otherSize = other.size();
+				reserve(otherSize);
+				_finish = _start + otherSize;
+				if (_start)
+					memcpy(_start, other._start, sizeof(T) * otherSize);
+			}
+			return *this;
+		}
+
+		~vector() {
+			delete[] _start;
+		}
+
 		size_t size() const { return _finish - _start; }
 		size_t capacity() const { return _endOfStorage - _start; }
 		
 		// 迭代器
-		iterator begin() { return _start };
-		const_iterator begin() const { return _start };
-		iterator end() { return _finish };
-		const_iterator end() const { return _finish };
+		iterator begin() { return _start; }
+		const_iterator begin() const { return _start; }
+		iterator end() { return _finish; }
+		const_iterator end() const { return _finish; }
 
-		void reserve(size_t n);
-		void resize(size_t n);
+		void reserve(size_t n) {
+			if (n > capacity()) {
+				T* tmp = new T[n];
+				size_t oldsize = size();
+				if (_start) {
+					memcpy(tmp, _start, sizeof(T) * oldsize);
+					delete[] _start;
+				}
+				_start = tmp;
+				_finish = _start + oldsize;
+				_endOfStorage = _start + n;
+			}
+		}
 
-		void push_back(const T& val);
+		void resize(size_t n, const T& val = T()) {
+			if (n > size()) {
+				size_t add = n - size();
+				checkCapacity(add);
+				while (add--) {
+					*_finish++ = val;
+				}
+			}
+			else {
+				_finish = _start + n;
+			}
+		}
 
-		T& operator[](size_t pos);
-		const T& operator[](size_t pos) const;
+		void push_back(const T& val) {
+			checkCapacity();
+			*_finish++ = val;
+		}
+
+		void pop_back() {
+			assert(_finish - 1 >= _start);
+			_finish--;
+		}
+
+		iterator insert(iterator pos, const T& val) {
+			assert(_start <= pos && pos <= _finish);
+			size_t len = pos - _start;
+			checkCapacity();
+			pos = _start + len;// 解决迭代器失效
+			memmove(pos + 1, pos, (_finish - pos) * sizeof(T));
+			*pos = val;
+			_finish++;
+
+			return pos;
+		}
+
+		iterator erase(iterator pos) {
+
+		}
+
+		T& operator[](size_t pos) {
+			assert(pos < size());
+			return _start[pos];
+		}
+
+		const T& operator[](size_t pos) const {
+			assert(pos < size());
+			return _start[pos];
+		}
 	private:
-		iterator _start;
-		iterator _finish;
-		iterator _endOfStorage;
+		iterator _start;// 指向数据块开始
+		iterator _finish;// 指向有效数据的尾
+		iterator _endOfStorage;// 指向存储容量的尾
+
+		void checkCapacity(size_t add = 0) {
+			size_t newCapacity = capacity();
+			if (_finish + add >= _endOfStorage) {
+				if (capacity() == 0) {
+					newCapacity = 4;// 初始空间大小
+					reserve(newCapacity);
+					if (_finish + add < _endOfStorage)
+						return;
+				}
+				while (size() + add >= newCapacity) {
+					newCapacity <<= 1;
+				}
+				reserve(newCapacity);
+			}
+		}
 	};
 }
--- test_7_9/test.cpp
+++ test_7_9/test.cpp
@@ -4,9 +4,21 @@
 
 using namespace std;
 
+template<typename T>
+void print(blus::vector<T>& v) {
+	for (auto e : v) {
+		cout << e << ' ';
+	}
+	cout << endl;
+}
+
 int main() {
 	blus::vector<int> v;
-	v.push_back(10);
+	v.push_back(1);
+	print<int>(v);
+
+	v.pop_back();
+	print<int>(v);
 
 	return 0;
 }
