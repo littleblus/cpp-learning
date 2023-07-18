535dbac85a024ea3ce2d918e3c491121ceafab82
littleblus <gqstudio004@foxmail.com>
2023-07-18 10:10:39 +0800

    链表

---
+++ 剑指offer专项突破/027.回文链表/test.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	bool isPalindrome(ListNode* head) {
+		// ·Ö¸î£¬·´×ª
+
+		ListNode* guard(new ListNode(-1, head)), * slow(guard), * fast(head);
+		while (fast && fast->next) {
+			fast = fast->next->next;
+			slow = slow->next;
+		}
+		fast = slow->next;
+		slow->next = nullptr;
+		ListNode* prev(nullptr), * cur(fast);
+		while (cur) {
+			ListNode* next = cur->next;
+			cur->next = prev;
+			prev = cur;
+			cur = next;
+		}
+		fast = prev;
+		while (head && fast) {
+			if (head->val != fast->val)
+				return false;
+			head = head->next;
+			fast = fast->next;
+		}
+		return true;
+	}
+};
