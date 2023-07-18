2af30667645c7c475d6e78f9636b34fe16222cc8
littleblus <gqstudio004@foxmail.com>
2023-07-18 16:21:20 +0800

    list的模拟实现

---
+++ test_7_16/myList.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace blus {
+	template<typename T>
+	class list {
+	public:
+		typedef list<T>* iterator;
+		typedef const list<T>* iterator;
+
+		list() {
+
+		}
+
+		~list() {
+
+		}
+
+	private:
+
+	};
+}
