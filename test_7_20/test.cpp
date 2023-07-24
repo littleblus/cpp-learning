fd388cf3ecb64946b8a363e57457ec6bb518957f
littleblus <gqstudio004@foxmail.com>
2023-07-24 17:25:43 +0800

    topK问题

--- test_7_20/test.cpp
+++ test_7_20/test.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int> pq(nums.begin(), nums.end());
-        while (k--) {
+        while (--k) {
             pq.pop();
         }
         return pq.top();
