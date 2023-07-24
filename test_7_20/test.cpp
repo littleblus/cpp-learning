6746c6f76c265446073069079bb7c125a7e823c7
littleblus <gqstudio004@foxmail.com>
2023-07-24 17:23:37 +0800

    topK问题

--- test_7_20/test.cpp
+++ test_7_20/test.cpp
@@ -1,34 +1,17 @@
-#include <iostream>
 #include <vector>
-#include "priority_queue.h"
+#include <queue>
 using namespace std;
 
-int main() {
-	vector<int> v;
-	v.push_back(15);
-	v.push_back(24);
-	v.push_back(3);
-	v.push_back(79);
-	v.push_back(57);
+// topK
+//https://leetcode.cn/problems/kth-largest-element-in-an-array/
 
-	blus::priority_queue<int> pq1(v.begin(), v.end());
-	while (!pq1.empty()) {
-		cout << pq1.top() << ' ';
-		pq1.pop();
-	}
-	cout << endl;
-
-	blus::priority_queue<int, vector<int>, greater<int>> pq2;
-	pq2.push(15);
-	pq2.push(24);
-	pq2.push(3);
-	pq2.push(79);
-	pq2.push(57);
-	while (!pq2.empty()) {
-		cout << pq2.top() << ' ';
-		pq2.pop();
-	}
-	cout << endl;
-
-	return 0;
-}
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        while (k--) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
