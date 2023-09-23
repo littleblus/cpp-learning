d70a1420134a38f1e0fc152deef0ccee70671b6a
刘伟峻 <gqstudio004@foxmail.com>
2023-09-23 11:28:18 +0800

    AVL树的验证，平衡因子的检测

--- test_9_6/AVLtree.h
+++ test_9_6/AVLtree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <assert.h>
+#include <queue>
 
 template<class T>
 class AVLtree {
@@ -70,10 +71,50 @@ class AVLtree {
 		}
 	}
 
-	void rotateRL(Node* root);
+	void rotateRL(Node* root) {
+		Node* tmp = root->_right->_left;
+		int& bf = tmp->_bf;
+		rotateR(root->_right);
+		rotateL(root);
+		if (abs(bf) == 1)
+			bf *= -1;
+	}
 
-	void rotateLR(Node* root);
+	void rotateLR(Node* root) {
+		int bf = root->_left->_right->_bf;
+		rotateR(root->_left);
+		rotateL(root);
+		if (abs(bf) == 1)
+			bf *= -1;
+	}
 
+	void printInorder_core(Node* root) {
+		if (root == nullptr)
+			return;
+		printInorder_core(root->_left);
+		std::cout << root->_val << ' ';
+		printInorder_core(root->_right);
+	}
+
+	bool isAVL_core(Node* root) {
+		if (root == nullptr)
+			return true;
+		else {
+			int bf = height(root->_right) - height(root->_left);
+			if (abs(bf) > 1) {
+				std::cout << "平衡因子异常：" << root->_val << " -> " << bf << std::endl;
+				return false;
+			}
+		}
+		return true;
+	}
+
+	int height(Node* root) {
+		if (root == nullptr)
+			return 0;
+		else
+			return std::max(height(root->_left), height(root->_right)) + 1;
+	}
 public:
 	bool insert(const T& val) {
 		Node* cur(_root), *parent(nullptr);
@@ -89,7 +130,9 @@ public:
 		}
 		// insert
 		cur = new Node(val);
-		if (val < parent->_val) {
+		if (parent == nullptr)
+			_root = cur;
+		else if (val < parent->_val) {
 			parent->_left = cur;
 		}
 		else {
@@ -111,20 +154,47 @@ public:
 				parent = parent->_father; // 继续向上调整
 			}
 			else if (parent->_bf == 2) {
-				// 旋转
-				if(cur->_bf == 1)
-					rotateL(parent);
+				if (cur->_bf == 1)
+					rotateL(parent); // 对parent进行左单旋
+				else // -1
+					rotateRL(parent); // 先右单旋，再左单旋
 				parent->_bf = cur->_bf = 0;
 			}
 			else if (parent->_bf == -2) {
-				if (cur->_bf == -1)
+				if (cur->_bf == -1) // 对parent进行右单旋
 					rotateR(parent);
+				else // 1
+					rotateLR(parent); // 先左单旋，再右单旋
 				parent->_bf = cur->_bf = 0;
 			}
 			else if (abs(parent->_bf) > 2)
 				assert(false);
 		}
 
+		return true;
+	}
+
+	void printInorder() {
+		printInorder_core(_root);
+		std::cout << std::endl;
+	}
+
+	bool isAVL() {
+		// bfs
+		std::queue<Node*> que;
+		que.push(_root);
+		while (!que.empty()) {
+			Node* cur = que.front();
+			que.pop();
+			if (isAVL_core(cur) == false)
+				return false;
+			else {
+				if (cur->_left)
+					que.push(cur->_left);
+				if (cur->_right)
+					que.push(cur->_right);
+			}
+		}
 		return true;
 	}
 private:
--- test_9_6/test.cpp
+++ test_9_6/test.cpp
@@ -1,6 +1,14 @@
 #include "AVLtree.h"
+#include <vector>
 using namespace std;
 
 int main() {
+	AVLtree<int> tree;
+	vector<int> v{ 5,8,3,4,0,36,2,14 };
+	for (auto e : v) {
+		tree.insert(e);
+		tree.isAVL();
+	}
+	tree.printInorder();
 	return 0;
 }
