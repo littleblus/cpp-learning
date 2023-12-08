1a621f72051f9cef89144c2e18d23f229ebe4512
littleblus <littleblus@163.com>
2023-12-08 22:47:17 +0800

    逆波兰表达式求值

--- 剑指offer专项突破/036.后缀表达式/test.cpp
+++ 剑指offer专项突破/036.后缀表达式/test.cpp
@@ -9,37 +9,24 @@ public:
 		stack<int> buf;
 		for (auto& e : tokens) {
 			if (e == "+" || e == "-" || e == "*" || e == "/") {
-				int res;
+				int res = buf.top();
+				buf.pop();
 				switch (e[0]) {
 				case '+':
-					res = buf.top();
-					buf.pop();
 					res += buf.top();
-					buf.pop();
-					buf.push(res);
 					break;
 				case '-':
-					res = -buf.top();
-					buf.pop();
-					res += buf.top();
-					buf.pop();
-					buf.push(res);
+					res = buf.top() - res;
 					break;
 				case '*':
-					res = buf.top();
-					buf.pop();
 					res *= buf.top();
-					buf.pop();
-					buf.push(res);
 					break;
 				case '/':
-					res = buf.top();
-					buf.pop();
 					res = buf.top() / res;
-					buf.pop();
-					buf.push(res);
 					break;
 				}
+				buf.pop();
+				buf.push(res);
 			}
 			else {
 				buf.push(stoi(e));
